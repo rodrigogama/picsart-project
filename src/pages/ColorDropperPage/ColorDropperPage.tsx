c5a0c9f2defce980642e70b2f8b954dc84b819bf
Rodrigo Gama <ro.gama91@gmail.com>
2024-05-17 13:46:24 +0200

    wip(pages): add dummy color dropper page

---
+++ src/pages/ColorDropperPage/ColorDropperPage.tsx
@@ -0,0 +1,9 @@
+import { PageShell } from "../../components/PageShell";
+
+export const ColorDropperPage = () => {
+  return (
+    <PageShell>
+      <h1>ColorDropperPage</h1>
+    </PageShell>
+  );
+};
---
+++ src/pages/ColorDropperPage/index.ts
@@ -0,0 +1,1 @@
+export { ColorDropperPage } from "./ColorDropperPage";
