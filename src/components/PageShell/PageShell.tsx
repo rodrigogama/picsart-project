cd01a1b0b30179f6d806c34009a35c6d9ba16231
Rodrigo Gama <ro.gama91@gmail.com>
2024-05-19 01:00:12 +0200

    fix(components): page shell dimension styles

--- src/components/PageShell/PageShell.tsx
+++ src/components/PageShell/PageShell.tsx
@@ -3,9 +3,9 @@ import { Navbar } from "../Navbar";
 
 export const PageShell: React.FC<PropsWithChildren> = ({ children }) => {
   return (
-    <main>
+    <main className="min-h-screen flex flex-col shrink-0">
       <Navbar />
-      <div className="mx-auto max-w-7xl px-4 py-4 sm:px-6 sm:py-6 lg:px-8 lg:py-8">
+      <div className="flex-1 w-full max-w-7xl px-4 py-4 sm:px-6 sm:py-6 lg:px-8 lg:py-8 self-center">
         {children}
       </div>
     </main>
